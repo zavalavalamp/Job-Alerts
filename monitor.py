7f7b108c074936d79b718421854814dcdfa08f89
zavalavalamp <markdzavalkov@gmail.com>
2026-09-17 13:56:40 -0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://jobs.nike.com"
+url = "https://careers.nike.com/jobs?filter%5Bcategory%5D%5B0%5D=Product%20Creation%2C%20Development%20%26%20Management"
 
 response = requests.get(url)
 
