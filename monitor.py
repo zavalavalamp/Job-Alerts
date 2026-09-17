c59b5cc0170c79c105f8cff6b4786a17be0e2edb
zavalavalamp <markdzavalkov@gmail.com>
2026-09-17 14:55:35 -0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,5 +1,17 @@
+import smtplib
 import os
+***m email.mime.text import MIMETex***sender = os.getenv("EMAIL_ADDRES***
+password = os.getenv("EMAIL_PAS***RD")
+recipient = os.getenv("EMAI***ECIPIENT")
 
-print("EMAIL_ADDRESS =", repr(os.getenv("EMAIL_ADDRESS")))
-print("EMAIL_PASSWORD =", "EXISTS" if os.getenv("EMAIL_PASSWORD") else "MISSING")
-print("EMAIL_RECIPIENT =", repr(os.getenv("EMAIL_RECIPIENT")))
+msg = MIMEText(
+    ***ccess!\n\nYour Nike Job Monitor ***il test worked."
+)
+
+msg["Subject***= "Nike Job Monitor Test"
+msg["F***"] = sender
+msg["To"] = recipien***with smtplib.SMTP_SSL("smtp.gmai***om", 465) as server:
+    server.***in(sender, password)
+    server***ndmail(sender, [recipient], msg.***string())
+
+print("Email sent suc***sfully")
