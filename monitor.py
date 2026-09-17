eaf0b6e635f0c6fb3bf8d8040560fb618302ee8d
zavalavalamp <markdzavalkov@gmail.com>
2026-09-17 13:54:06 -0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,1 +1,8 @@
-print("monitor running")
+import requests
+
+url = "https://jobs.nike.com"
+
+response = requests.get(url)
+
+print("Status code:", response.status_code)
+print("Page length:", len(response.text))
