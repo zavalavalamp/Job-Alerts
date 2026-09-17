9411d3ef50f0c74865746cd11c6915ed5b4b68ce
zavalavalamp <markdzavalkov@gmail.com>
2026-09-17 14:16:14 -0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -5,12 +5,9 @@ url = "https://careers.nike.com/jobs?filter%5Bcategory%5D%5B0%5D=Product%20Creat
 
 html = requests.get(url).text
 
-job_ids = set(re.findall(r'R-\d+', html))
+job_ids = sorted(set(re.findall(r'R-\d+', html)))
 
-print("Found", len(job_ids), "jobs")
+print("Found", len(job_ids), "unique jobs")
 
-with open("seen_jobs.txt", "w") as f:
-    for job in sorted(job_ids):
-        f.write(job + "\n")
-
-print("Saved all job IDs")
+for job in job_ids:
+    print(job)
