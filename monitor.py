b92dc17ed0bcf5e08860740ea4e73ed65a6c72c8
zavalavalamp <markdzavalkov@gmail.com>
2026-09-17 14:44:20 -0700

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,22 +1,22 @@
 import smtplib
 import os
+from email.message import EmailMessage
 
 sender = os.getenv("EMAIL_ADDRESS")
 password = os.getenv("EMAIL_PASSWORD")
 recipient = os.getenv("EMAIL_RECIPIENT")
 
-subject = "Nike Job Monitor Test"
+msg = EmailMessage()
+msg["Subject"] = "Nike Job Monitor Test"
+msg["From"] = sender
+msg["To"] = recipient
 
-body = """
-Success!
-
-If you received this email, GitHub Actions can send Gmail notifications successfully.
-"""
-
-message = f"Subject: {subject}\n\n{body}"
+msg.set_content(
+    "Success!\n\nIf you received this email, your Nike Job Monitor email notifications are working."
+)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
     smtp.login(sender, password)
-    smtp.sendmail(sender, recipient, message)
+    smtp.send_message(msg)
 
 print("Email sent successfully")
